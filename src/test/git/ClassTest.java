ac1e6543d987c25a4a8bd0a56363639a81e5789b
Vincent DERECLENNE <v.dereclenne@gmail.com>
2020-04-15 16:55:00 +0200

    Commit and push test moi

--- src/test/git/ClassTest.java
+++ src/test/git/ClassTest.java
@@ -1,13 +1,8 @@
 package test.git;
 
 public class ClassTest {
-<<<<<<< HEAD
 	// Kévin
 	// Jeremy
 	// Moi
-=======
-	//Kévin
-	//Jeremy
->>>>>>> branch 'master' of https://github.com/g32-team-java/projet.git
 	
 }
