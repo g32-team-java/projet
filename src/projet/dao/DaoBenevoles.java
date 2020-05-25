058193d7a047cb90bb3862176c651f36165bdafb
kevinZENOU <DarkA@PCKZ>
2020-05-25 11:26:49 +0200

    .

--- src/projet/dao/DaoBenevoles.java
+++ src/projet/dao/DaoBenevoles.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,7 +12,6 @@ import javax.sql.DataSource;
 
 import jfox.dao.jdbc.UtilJdbc;
 import projet.data.Benevoles;
-import projet.data.Memo;
 
 public class DaoBenevoles {
 	
