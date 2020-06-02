db77348b86e3a222372e72b1faa870af55f51a5e
JeremyDESCOMBE <descombj@3il.fr>
2020-06-02 17:00:16 +0200

    .

--- src/projet/view/benevoles/ModelBenevoles.java
+++ src/projet/view/benevoles/ModelBenevoles.java
@@ -17,99 +17,85 @@ import projet.view.systeme.ModelConfig;
 
 public class ModelBenevoles {
 
-	private final ObservableList<Benevoles> liste = FXCollections.observableArrayList(); 
-	private final ObservableList<String> details = FXCollections.observableArrayList(); 
+	private final ObservableList<Benevoles> liste = FXCollections.observableArrayList();
+	private final ObservableList<String> details = FXCollections.observableArrayList();
 	private final Property<Boolean> majeur = new SimpleObjectProperty<Boolean>();
 	private final Property<Boolean> permis = new SimpleObjectProperty<Boolean>();
 	private final Benevoles courant = new Benevoles();
 
-	
 // Autres champs
-    @Inject
-	private IMapper			mapper;
-    @Inject
-	private DaoBenevoles	daoBenevoles;
-    @Inject
-    private ModelConfig		modelConfig;
-    
+	@Inject
+	private IMapper mapper;
+	@Inject
+	private DaoBenevoles daoBenevoles;
+	@Inject
+	private ModelConfig modelConfig;
+
 // Getters
- 	public ObservableList<Benevoles> getListe() {
- 		return liste;
- 	}
- 	
- 	public ObservableList<String> getDetails() {
- 		return details;
- 	}
- 	
- 	public Benevoles getCourant() {
- 		return courant;
- 	}
- 	
- 	public Property<Boolean> majeurProperty() {
+	public ObservableList<Benevoles> getListe() {
+		return liste;
+	}
+
+	public ObservableList<String> getDetails() {
+		return details;
+	}
+
+	public Benevoles getCourant() {
+		return courant;
+	}
+
+	public Property<Boolean> majeurProperty() {
 		return majeur;
 	}
- 	
- 	public Property<Boolean> permisProperty() {
+
+	public Property<Boolean> permisProperty() {
 		return permis;
 	}
- 	
-// Actualisations
- 	public void actualiserListe() {
- 		liste.setAll( daoBenevoles.listerTout() );
- 		
-  	}
- 	
- 	public void actualiseLister(Benevoles item) {
- 		Benevoles LB = daoBenevoles.lister(item.getId());
- 		String id = LB.getId().toString();
- 		String nom = LB.getNom();
- 		String prenom = LB.getPrenom();
- 		String mail = LB.getMail();
- 		String tel = LB.getTelephone().toString();
 
- 		permis.setValue(LB.getPermis());
- 		majeur.setValue(LB.getMajeur());
- 		
- 		List<String> liste = new LinkedList<>();
- 		liste.add(id);
- 		liste.add(nom);
- 		liste.add(prenom);
- 		liste.add(mail);
- 		liste.add(tel);
- 		
- 		details.setAll(liste);
- 				
-  	}
- 	
- 	public void preparerModifier(Benevoles item) {
- 		mapper.update(courant, daoBenevoles.retrouverBenevoles(item.getId()));
- 	}
- 	
- 	public void preparerLister(Benevoles item) {
- 		mapper.update(courant, daoBenevoles.lister(item.getId()));
- 	}
- 	 	
- 	@PostConstruct
-	public void init()
-	{
+// Actualisations
+	public void actualiserListe() {
+		liste.setAll(daoBenevoles.listerTout());
+
+	}
+
+	public void actualiseLister(Benevoles item) {
+		Benevoles LB = daoBenevoles.lister(item.getId());
+		String id = LB.getId().toString();
+		String nom = LB.getNom();
+		String prenom = LB.getPrenom();
+		String mail = LB.getMail();
+		String tel = LB.getTelephone().toString();
+
+		permis.setValue(LB.getPermis());
+		majeur.setValue(LB.getMajeur());
+
+		List<String> liste = new LinkedList<>();
+		liste.add(id);
+		liste.add(nom);
+		liste.add(prenom);
+		liste.add(mail);
+		liste.add(tel);
+
+		details.setAll(liste);
+
+	}
+
+	public void preparerModifier(Benevoles item) {
+		mapper.update(courant, daoBenevoles.retrouverBenevoles(item.getId()));
+	}
+
+	public void preparerLister(Benevoles item) {
+		mapper.update(courant, daoBenevoles.lister(item.getId()));
+	}
+
+	@PostConstruct
+	public void init() {
 		actualiserListe();
 	}
- 	
-	public void supprimer( Benevoles item ) {
-		Benevoles benevole = daoBenevoles.lister(item.getId());
-		int idb = benevole.getId();
-		daoBenevoles.supprimer(idb);
-		mapper.update( courant, UtilFX.findNext( liste, item ) );
-		getFichierSchemaCourant().delete();
+
+	public void supprimer(Benevoles item) {
+		daoBenevoles.supprimer(item.getId());
+		mapper.update(courant, UtilFX.findNext(liste, item));
 	}
-	
-	// Méthodes auxiliaires
-	
-	public File getFichierSchemaCourant() {
-		String nomFichier = String.format( "%06d.jpg", courant.getId() );
-		File dossierSchemas = modelConfig.getDossierSchemas();
-		return new File( dossierSchemas, nomFichier );
-	}
-	
-	
+
 }
